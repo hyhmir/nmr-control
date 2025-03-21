4d669a4c5cc347e15d5698615b56300ac8739cd7
hyh <samo.krejan@gmail.com>
2025-03-21 14:49:54 +0100

    added main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+##################################
+# RUN THIS FILE TO START THE APP #
+##################################
+
+# import everything necessary
+
+
+
+if __name__ == '__main__':
+    # initialize classes and run the code
+    print('Congrats!!!')
